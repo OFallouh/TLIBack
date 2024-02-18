8d6e1b2a17e617d3e43134dc18c325c832df8e50
Roquia <hp@DESKTOP-1F41176>
2024-02-18 04:15:40 -0800

    update

--- TLIS_API/Controllers/SiteController.cs
+++ TLIS_API/Controllers/SiteController.cs
@@ -26,6 +26,7 @@ using TLIS_Service.ServiceBase;
 
 namespace TLIS_API.Controllers
 {
+    [ServiceFilter(typeof(WorkFlowMiddleware))]
     [ServiceFilter(typeof(LogFilterAttribute))]
     [Route("api/[controller]")]
     [ApiController]
